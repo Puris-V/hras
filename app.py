2972143b4bdd4d6c3fb7015f8b00c7fb31913f5b
Puris-V <purisvadim@gmail.com>
2025-04-10 20:51:02 +0100

    Update app.py
    добавил логгинг

--- app.py
+++ app.py
@@ -3,6 +3,7 @@ from fastapi.responses import HTMLResponse
 from fastapi.templating import Jinja2Templates
 from pydantic import BaseModel
 from risk_assessment import main as risk_assessment_main
+import logging
 
 app = FastAPI()
 
